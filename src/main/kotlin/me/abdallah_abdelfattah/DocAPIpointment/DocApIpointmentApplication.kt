1018181e7dcb7bbc9b9e282b20c1738f3fa4eba8
AbdAllah <abdallah.boda13@gmail.com>
2025-01-22 00:24:18 +0200

    add the init project setup

---
+++ src/main/kotlin/me/abdallah_abdelfattah/DocAPIpointment/DocApIpointmentApplication.kt
@@ -0,0 +1,11 @@
+package me.abdallah_abdelfattah.DocAPIpointment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DocApIpointmentApplication
+
+fun main(args: Array<String>) {
+	runApplication<DocApIpointmentApplication>(*args)
+}
---
+++ src/test/kotlin/me/abdallah_abdelfattah/DocAPIpointment/DocApIpointmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.abdallah_abdelfattah.DocAPIpointment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DocApIpointmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
