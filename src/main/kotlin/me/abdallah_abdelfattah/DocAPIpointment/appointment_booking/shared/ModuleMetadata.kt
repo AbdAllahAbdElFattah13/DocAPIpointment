50c980c5b1546161dcd9227ea8b1ef53dc0530a5
AbdAllah <abdallah.boda13@gmail.com>
2025-01-26 13:43:53 +0200

    add the 5 modules with the shared pacakges

---
+++ src/main/kotlin/me/abdallah_abdelfattah/DocAPIpointment/appointment_booking/shared/ModuleMetadata.kt
@@ -0,0 +1,12 @@
+package me.abdallah_abdelfattah.DocAPIpointment.appointment_booking.shared
+
+import org.springframework.modulith.ApplicationModule
+import org.springframework.modulith.NamedInterface
+import org.springframework.modulith.PackageInfo
+
+
+@PackageInfo
+@NamedInterface("appointment_booking.shared")
+@ApplicationModule(type = ApplicationModule.Type.OPEN)
+interface ModuleMetadata {
+}
---
+++ src/main/kotlin/me/abdallah_abdelfattah/DocAPIpointment/doctor_appointment_management/shared/ModuleMetadata.kt
@@ -0,0 +1,12 @@
+package me.abdallah_abdelfattah.DocAPIpointment.doctor_appointment_management.shared
+
+import org.springframework.modulith.ApplicationModule
+import org.springframework.modulith.NamedInterface
+import org.springframework.modulith.PackageInfo
+
+
+@PackageInfo
+@NamedInterface("doctor_appointment_management.shared")
+@ApplicationModule(type = ApplicationModule.Type.OPEN)
+interface ModuleMetadata {
+}
